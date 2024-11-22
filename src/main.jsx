4135575cda3301fd189684012850f2b8ad93e2ad
luketucich <ltucich@gmail.com>
2024-11-21 23:46:50 -0500

    style: Removed left margin for mobile displays to see if it looks better.

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,7 @@ import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <div
-      className="flex flex-col items-center font-sans 
-          [@media(max-width:500px)]:ml-72"
-    >
+    <div className="flex flex-col items-center font-sans ">
       <h1 className="font-semibold text-white text-6xl tracking-tight p-8 drop-shadow-lg">
         EasyCV
       </h1>
